335c35ca1b8150b516da1dc6fb23919c20b41b49
Karl Kalinowski "Guybrush Threepwood <mukamuka777@aol.com>
2015-05-14 09:43:48 -0500

    6 rooms again

--- header/main.h
+++ header/main.h
@@ -23,6 +23,8 @@
 #include "Backgrounds/Room2.map.c"
 #include "Backgrounds/Room3.map.c"
 #include "Backgrounds/Room4.map.c"
+#include "Backgrounds/Room5.map.c"
+#include "Backgrounds/Room6.map.c"
 #include "Backgrounds/Room1.raw.c"
 #include "Backgrounds/Room1hitmap.map.c"
 #include "Backgrounds/Room1hitmap.raw.c"
@@ -229,6 +231,18 @@ void loadRoomData() {
     room4->mapData = Room4_Map;
     room4->hitmapData = Room4hitmap_Map;
     room4->numEnemies = 5;
+    
+    roomData* room5 = (roomData*) malloc(sizeof(roomData));
+    room5->spriteData = roomSprites;
+    room5->mapData = Room5_Map;
+    room5->hitmapData = Room5hitmap_Map;
+    room5->numEnemies = 5;
+
+    roomData* room6 = (roomData*) malloc(sizeof(roomData));
+    room6->spriteData = roomSprites;
+    room6->mapData = Room6_Map;
+    room6->hitmapData = Room6hitmap_Map;
+    room6->numEnemies = 5;
 
 
     room1->roomLeft = room4;
@@ -248,8 +262,18 @@ void loadRoomData() {
 
     room4->roomLeft = room4;
     room4->roomRight= room1;
-    room4->roomUp = room4;
+    room4->roomUp = room5;
     room4->roomDown = room4;
+    
+    room5->roomLeft = room5;
+    room5->roomRight= room5;
+    room5->roomUp = room6;
+    room5->roomDown = room4;
+
+    room6->roomLeft = room6;
+    room6->roomRight= room6;
+    room6->roomUp = room6;
+    room6->roomDown = room5;
 
     currentroom = room1;
 }
